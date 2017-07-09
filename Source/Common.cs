c4cdee52ab95ba9a99f84510e16fa34eb8a583e5
alexguirre <alexguirre.aa@gmail.com>
2017-07-10 01:09:08 +0200

    Implement MenuItemScroller(base, Enum and Numeric classes)

--- Source/Common.cs
+++ Source/Common.cs
@@ -2,6 +2,8 @@
 {
     using System.IO;
 
+    using Rage;
+
     internal static class Common
     {
         public const string ResourcesFolder = @"RAGENativeUI Resources\";
@@ -10,6 +12,11 @@
         {
             if (!Directory.Exists(ResourcesFolder))
                 Directory.CreateDirectory(ResourcesFolder);
+        }
+
+        public static float GetFontHeight(string fontName, float fontSize)
+        {
+            return Graphics.MeasureText("A", fontName, fontSize).Height;
         }
     }
 }
--- Source/Menus/MenuItem.cs
+++ Source/Menus/MenuItem.cs
@@ -13,7 +13,7 @@
         public string Text { get; set; }
         public SizeF Size { get; set; } = new SizeF(432f, 37f);
 
-        public float TextSafezone { get; set; } = 11.5f;
+        public float TextSafezone { get; set; } = 8.25f;
 
         public MenuItem(string text)
         {
