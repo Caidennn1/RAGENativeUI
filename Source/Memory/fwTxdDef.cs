1a33bac0a5755defcd1fca31ff6dc300b1fa5702
alexguirre <alexguirre.aa@gmail.com>
2017-08-07 07:11:59 +0200

    Add TextureDictionary.TextureNames

---
+++ Source/Memory/fwTxdDef.cs
@@ -0,0 +1,10 @@
+﻿namespace RAGENativeUI.Memory
+{
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Explicit, Size = 24)]
+    internal unsafe struct fwTxdDef
+    {
+        [FieldOffset(0x0000)] public pgDictionary_grcTexture* TexturesDictionary;
+    }
+}
