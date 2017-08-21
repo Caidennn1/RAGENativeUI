5b866f04f00593ebb0c7a98d0d1c2c9c4fcbc2b8
alexguirre <alexguirre.aa@gmail.com>
2017-08-21 02:33:27 +0200

    Add Scaleform.BoundingBoxColor

--- Source/Memory/GFx/GFxMovieRoot.cs
+++ Source/Memory/GFx/GFxMovieRoot.cs
@@ -6,5 +6,10 @@
     [StructLayout(LayoutKind.Explicit)]
     internal unsafe struct GFxMovieRoot
     {
+
+        [FieldOffset(0x09A0)] public byte BackgroundColorBlue;
+        [FieldOffset(0x09A1)] public byte BackgroundColorGreen;
+        [FieldOffset(0x09A2)] public byte BackgroundColorRed;
+        [FieldOffset(0x09A3)] public byte BackgroundColorAlpha;
     }
 }
